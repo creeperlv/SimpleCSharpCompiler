db0dd706f82f476cbb38b373bebe738885f56109
Creeper Lv <1376021184@qq.com>
2019-12-29 20:16:47 +0800

    Fix install script.

--- Install.Linux.sh
+++ Install.Linux.sh
@@ -4,5 +4,5 @@ cd temp
 git clone https://github.com/creeperlv/SimpleCSharpCompiler.git
 cd SimpleCSharpCompiler
 dotnet build
-mv -rf SimpleCSharpCompiler/bin/Debug/netcoreapp3.1/ /usr/share/SimpleCSharpCompiler/
+mv -f SimpleCSharpCompiler/bin/Debug/netcoreapp3.1/ /usr/share/SimpleCSharpCompiler/
 sudo ln -s /usr/share/SimpleCSharpCompiler/scsc /usr/bin/scsc
