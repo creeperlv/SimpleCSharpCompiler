cd001b6e50983c71d99aa480684f27aba5983467
Creeper Lv <1376021184@qq.com>
2019-12-30 00:43:31 +0800

    Update install script. Fix prebuilt host.

--- Install.Linux.sh
+++ Install.Linux.sh
@@ -4,6 +4,9 @@ cd temp
 git clone https://github.com/creeperlv/SimpleCSharpCompiler.git
 cd SimpleCSharpCompiler
 dotnet build
+#Set Hosts to 777 permission.
+chmod 777 SimpleCSharpCompiler/bin/Debug/netcoreapp3.1/PreBuilt
+chmod 777 SimpleCSharpCompiler/bin/Debug/netcoreapp3.1/PreBuilt/*
 mv -f SimpleCSharpCompiler/bin/Debug/netcoreapp3.1/ /usr/share/SimpleCSharpCompiler/
 sudo ln -s /usr/share/SimpleCSharpCompiler/scsc /usr/bin/scsc
 cd ..
